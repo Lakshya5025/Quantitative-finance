0d39657532b474a88948c24be2f258142dc3e21a
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-11-05 15:39:24 +0530

    complete tasks for day1

---
+++ quant-week1/day1/day1.py
@@ -0,0 +1,82 @@
+# print("hello")
+
+# user_name = input("Please enter you name: ")
+
+# print(user_name)
+
+# print("Thank you for you proving you name")
+
+# num_1 = int(input("Enter the first number : "));
+# num_2 = int(input("Enter the second number : "));
+
+# print(f"{num_1} + {num_2} = {num_1 + num_2} ")
+
+
+
+# num = int(input("Enter the number : "));
+# message = "";
+# if (num % 2 == 0):
+#     message = "even";
+# else:
+#     message = "odd"
+
+# print(f"{num} is {message}");
+
+
+
+# reverse a list
+# s = ["h","e","l","l","o"]
+# end = len(s) - 1;
+# start = 0;
+# while(start < end):
+#     s[start], s[end] = s[end], s[start];
+#     start+=1;
+#     end-=1;
+
+
+# Find max of 3 numbers
+
+# a = 5;
+# b = 3;
+# c = 7;
+
+# biggest:int
+# if(a > b and a > c): biggest = a;
+# elif(b > a and b > c): biggest = b;
+# elif(c > a and c > b): biggest = c;
+# else: biggest = -1;
+# print(biggest);
+
+
+# Count vowels in a word
+
+# s = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+# count = 0;
+# for a in s:
+#     if(a == 'a' or a == 'e' or a == 'i' or a == 'o' or a == 'u'): count += 1;
+
+# print(count);
+
+
+# Swap two variables without a third variable
+
+# a = 5;
+# b = 8;
+
+# a = a + b;
+# b = a - b;
+# a = a - b;
+# print(a , " " , b);
+
+
+# Check if a number is prime
+
+# num = 10;
+
+# isPrime = True;
+
+# for i in range(2, int(num/2)):
+#     if(num % i == 0):
+#         isPrime = False
+#         break;
+# print(isPrime)
---
+++ quant-week1/day1/readme.md
@@ -0,0 +1,19 @@
+### ✅ DAY 1 — Setup + Basics
+
+**Tasks**
+
+- Install:
+  - Python 3.x
+  - VS Code or PyCharm
+  - pip
+- Write these programs from scratch:
+  - ✅ Print your name
+  - ✅ Add two numbers
+  - ✅ Ask for user input and print a greeting
+  - ✅ Check if a number is even or odd
+- Solve problems:
+  - ✅ Reverse a string
+  - ✅ Find max of 3 numbers
+  - ✅ Count vowels in a word
+  - ✅ Swap two variables without a third variable
+  - ✅ Check if a number is prime
