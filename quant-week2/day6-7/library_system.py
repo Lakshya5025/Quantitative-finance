3ed12e0875f3b77224f2bea6f77354c502d3e25b
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-11-17 14:43:51 +0530

    Implement add member and add book functionality

---
+++ quant-week2/day6-7/library_system.py
@@ -0,0 +1,126 @@
+import os, json
+import random
+curr_dir = os.getcwd()
+data_folder_path = os.path.join(curr_dir, "quant-week2\\day6-7\\data")
+books_records = os.path.join(data_folder_path, "books.json")
+members_records = os.path.join(data_folder_path, "members.json")
+
+class InvalidAgeException(Exception):
+    def __init__(self, *args):
+        self.message = "Age must be in range of 7 to 100"
+        super().__init__(*args)
+class InvalidBalanceException(Exception):
+    def __init__(self, *args):
+        self.message = "Balance must be positive number"
+        super().__init__(*args)
+class InsuffecientBalanceException(Exception):
+    def __init__(self, *args):
+        self.message = "Insuffecient balance"
+        super().__init__(*args)
+class InvalidCopies(Exception):
+    def __init__(self, *args):
+        self.message = "Number of copies must be positive"
+        super().__init__(*args)
+
+def memberNumberGenerator():
+    member_number = random.randint(1000000000,9999999999)
+    return member_number 
+            
+def createJSONOfBook(book, book_id):
+    return {
+    "id": book_id + 1 ,
+    "title": book.title,
+    "author": book.author,
+    "rent_price": book.rent_price,
+    "price": book.price,
+    "copies": book.copies,
+    "left_copies": book.copies
+  }
+    
+def createJSONOfMember(member):
+    return {
+        "membership_number" : member.membership_number,
+        "name" : member.name,
+        "age" : member.age,
+        "balance" : member.balance,
+        "rented_books" : [],
+        "bought_books" : []
+    }
+
+def getAllBooks():
+    with open(books_records, "r", encoding='utf-8') as f:
+        available_books_list = json.load(f)
+        for book_details in available_books_list:
+            if(book_details['left_copies'] > 0):
+                print(f"Id:{book_details['id']}, {book_details['title']}, {book_details['author']}, Price:{book_details['price']}, Rent:{book_details['rent_price']}")
+  
+
+class Member():
+    def __init__(self, name, age, balance):
+        self.name = name
+        self.age = age
+        self.balance = balance - 50
+        self.membership_number = memberNumberGenerator()
+    def save_member(self):
+        if(self.age >= 7 and self.age <= 100):
+            if(self.balance >= 0):
+                with open(members_records, "r", encoding='utf-8') as f:
+                    members = json.load(f)
+                    members.append(createJSONOfMember(self))
+                with open(members_records, "w", encoding='utf-8') as f:
+                    json.dump(members, f, indent=2)
+            else:
+                raise InvalidBalanceException()
+        else:
+            raise InvalidAgeException()
+            
+class Book():
+    def __init__(self, title, author, price, rent_price, copies):
+        self.title = title
+        self.author = author
+        self.price = price
+        self.rent_price = rent_price
+        self.copies = copies
+    
+    def save_books_details(self):
+        if(self.copies > 0):
+            with open(books_records, "r", encoding='utf-8') as f:
+                available_books_list = json.load(f)
+                total_books = len(available_books_list)
+                available_books_list.append(createJSONOfBook(self,total_books))
+            with open(books_records, "w", encoding="utf-8") as f:
+                json.dump(available_books_list, f, indent=2)
+        else:
+            raise InvalidCopies()
+                
+
+print("Operations you can perform:-\n1.Add new book\n2.Add new member\n3.Available Books")
+user_choice = input("Enter you choice: ")
+if(user_choice  == '1'):
+    title = input("Enter book title: ")
+    author = input("Enter book author: ")
+    price = int(input("Enter book price: "))
+    rent_price = int(input("Enter book rent price: "))
+    copies = int(input('Enter number of copies: '))
+    new_book = Book(title=title, author=author, price=price, rent_price=rent_price, copies=copies)
+    try:
+        new_book.save_books_details()
+    except InvalidCopies as e:
+        print(e.message)
+elif(user_choice == '2'):
+    name = input("Enter member name: ")
+    age = int(input("Enter member age: "))
+    balance = int(input("Enter user balance: "))
+    new_member = Member(name=name, age=age, balance=balance)
+    try:
+        new_member.save_member()
+    except InvalidAgeException as e:
+        print(e.message)
+    except InvalidBalanceException as e:
+        print(e.message)
+
+elif(user_choice == '3'):
+    getAllBooks()
+
+
+# Implement rent, return and purchase feature
---
+++ quant-week2/day6-7/readme.md
@@ -0,0 +1,8 @@
+### ✅ DAY 6–7 — Challenge Project
+
+**library_system.py**
+
+- ✅ Book class (title, author, price, copies)
+- ✅ Add, issue, return books
+- ✅ Save to file
+- ✅ Handle missing files
